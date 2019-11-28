da467f37187d65dcb787864555e206b0176a6690
Eberhard Beilharz <eb1@sil.org>
2019-11-28 15:53:40 +0100

    Don't specify ToolsVersion for MSBuild task
    msbuild 16.0 on Ubuntu has a bug
    (https://github.com/microsoft/msbuild/issues/4535) that fails the
    build if we specify `ToolsVersion="15.0"`. Since this is not really
    necessary we omit it.
    
    This change is in preparation for Mono 6.
    
    Change-Id: Ia48fda3353f852b43b23e65d5a3e368510927613

--- Build/Src/FwBuildTasks/CollectTargets.cs
+++ Build/Src/FwBuildTasks/CollectTargets.cs
@@ -363,9 +363,8 @@ namespace FwBuildTasks
 						// <MsBuild> task
 						writer.WriteLine("\t\t<MSBuild Projects=\"{0}\"", m_mapProjFile[project].Replace(m_fwroot, "$(fwrt)"));
 						writer.WriteLine("\t\t\tTargets=\"$(msbuild-target)\"");
-						writer.WriteLine("\t\t\tProperties=\"$(msbuild-props);IntermediateOutputPath=$(dir-fwobj){0}{1}{0};DefineConstants=$({2}Defines);$(warningsAsErrors);WarningLevel=4;LcmArtifactsDir=$(LcmArtifactsDir)\"",
+						writer.WriteLine("\t\t\tProperties=\"$(msbuild-props);IntermediateOutputPath=$(dir-fwobj){0}{1}{0};DefineConstants=$({2}Defines);$(warningsAsErrors);WarningLevel=4;LcmArtifactsDir=$(LcmArtifactsDir)\"/>",
 							Path.DirectorySeparatorChar, GetProjectSubDir(project), project);
-						writer.WriteLine("\t\t\tToolsVersion=\"{0}\"/>", toolsVersion);
 						// <Clouseau> verification task
 						writer.WriteLine("\t\t<Clouseau Condition=\"'$(Configuration)' == 'Debug'\" AssemblyPathname=\"$(dir-outputBase)/{0}\"/>", AssemblyName);
 
